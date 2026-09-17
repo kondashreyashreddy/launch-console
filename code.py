c90d91c26a2cb756d589c3b1fb9aed33007d2a84
Shreyash Reddy Konda <kondashreyashreddy@gmail.com>
2026-09-16 22:22:02 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,19 @@
 name = input("What's your name? ")
 print(f"Welcome {name} to Shreyash's Launch Console!")
 
-print("1) About me")
-print("2) My goals")
-print("3) Exit")
-choice = input("Pick 1-3: ")
+print("1) About Me")
+print("2) My Goals")
+print("3) 26-27 Coursework @ High School")
+print("4) Exit")
+choice = input("Pick 1-4: ")
 
 if choice == "1":
   print("I am Shreyash Reddy Konda, a freshman student at Round Rock High School in Round Rock, Texas.")
 elif choice == "2":
   print("My goals are to have a degree in Computer Engineering and to master taekwondo.")
 elif choice == "3":
+  print("My STEM coursework for the 2026-2027 school year at Round Rock High School is AP Computer Science Principles, Honors Algebra II, Honors Biology, and Introduction to Engineering Design.")
+elif choice == "4":
   print("Goodbye!")
-  running = False
 else:
-  print("Please pick 1, 2, or 3.")
+  print("Please pick 1, 2, 3, or 4.")
